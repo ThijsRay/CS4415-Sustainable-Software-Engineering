27f499117034ff5bb12c0a1955c5b565f00e0ed4
Jeffrey Bouman <jeffrey.bouman@hotmail.com>
2022-03-15 11:31:24 +0100

    Setup for a nicer implementation of RAPL

--- project2/src/sensors/mod.rs
+++ project2/src/sensors/mod.rs
@@ -1,14 +1,134 @@
-use std::fs;
+use std::fs::read_to_string;
+use std::time::{Duration, Instant};
+use std::result::Result;
+use std::option::Option;
+
+// Trait for all kind of sensors to implement
+pub trait Sensor {
+    // Start position to measure the power consumption and timer
+    fn start_measuring(&mut self);
+    // Stop position to measure the power consumption and timer
+    fn stop_measuring(&mut self);
+    // Retrieve the final value from the sensor, AFTER start and stop
+    fn get_measured_uj(&self) -> u128;
+    // Retrieve the elapsed time between start and stop call
+    fn get_elapsed_time_us(&self) -> u128;
+    // Retrieve a duration value instead of seconds directly
+    fn get_duration(&self) -> Duration;
+}
+
+struct RAPLSensor {
+    location: String,
+    // Timer values
+    timer_start_position: Option<Instant>,
+    timer_end_position: Option<Instant>,
+    // Energy values
+    energy_start_position: u128,
+    energy_end_position: u128,
+    energy_max_range: u128,
+}
+
+// Sensor trait implementation for RAPLSensor
+impl Sensor for RAPLSensor {
+    fn start_measuring(&mut self){
+        let measuring_location = self.location.to_string() + "/energy_uj";
+        // Access rights and enabling is checked at initialization of sensor so unwrap is allowed
+        let current_measured_uj = read_to_string(measuring_location).unwrap();
+        self.energy_start_position = RAPLSensor::convert_read_string_to_u128(current_measured_uj);
+
+        self.timer_start_position = Some(Instant::now());
+    }
+
+    fn stop_measuring(&mut self){
+        let measuring_location = self.location.to_string() + "/energy_uj";
+        let current_measured_uj = read_to_string(measuring_location).unwrap();
+        self.energy_end_position = RAPLSensor::convert_read_string_to_u128(current_measured_uj);
+
+        self.timer_end_position = Some(Instant::now());
+    }
+
+    fn get_measured_uj(&self) -> u128{
+        if self.energy_end_position < self.energy_start_position {
+            return (self.energy_max_range - self.energy_start_position) + self.energy_end_position;
+        } else {
+            return self.energy_start_position - self.energy_end_position;
+        }
+    }
+
+    fn get_elapsed_time_us(&self) -> u128 {
+        // Check whether both instants are set correctly
+        if self.timer_start_position.is_none() || self.timer_end_position.is_none() {
+            return 0;
+        }
+        // Now unwrappable due to above check
+        let start = self.timer_start_position.unwrap();
+        let end = self.timer_end_position.unwrap();
+        return end.duration_since(start).as_micros();
+    }
+
+    fn get_duration(&self) -> Duration {
+        // Check whether both instants are set correctly
+        if self.timer_start_position.is_none() || self.timer_end_position.is_none() {
+            return Duration::new(0, 0);
+        }
+        // Now unwrappable due to above check
+        let start = self.timer_start_position.unwrap();
+        let end = self.timer_end_position.unwrap();
+        return end.saturating_duration_since(start);
+    }
+}
+
+// Implementation of RAPLSensor sepcific functions
+impl RAPLSensor {
+    pub fn new(location: String) -> Result<RAPLSensor, &'static str> {
+        let enabled_location = location.to_string() + "/enabled";
+        let enabled = read_to_string(enabled_location);
+        // Check whether the location is actually reachable
+        if enabled.is_err() {
+            return Err("Given location is unreachable");
+        }
+        // Check if RAPL is enabled at this location
+        // No error received to unwrap is possible
+        if !enabled.unwrap().starts_with("1") {
+            return Err("RAPL sensor is not enabled");
+        }
+        // Check whether permission is set correctly of the measuring location
+        let measuring_location = location.to_string() + "/energy_uj";
+        let measured_energy = read_to_string(measuring_location);
+        if measured_energy.is_err() {
+            return Err("Missing rights to read from /energy_uj");
+        }
+        // Retrieve the max range value of the sensor
+        let max_range_location = location.to_string() + "max_energy_range_uj";
+        let max_range_string = read_to_string(max_range_location).unwrap();
+        let max_range = RAPLSensor::convert_read_string_to_u128(max_range_string);
+
+        Ok(RAPLSensor {
+            location: location,
+            timer_start_position: None,
+            timer_end_position: None,
+            energy_start_position: 0,
+            energy_end_position: 0,
+            energy_max_range: max_range,
+        })
+    }
+
+    // TODO implement
+    // Input should look like "xxxxxxxxx\n"
+    fn convert_read_string_to_u128(input_string: String) -> u128 {
+        return 0;
+    }
+}
 
 pub fn start(){
-    let enabled = fs::read_to_string("/sys/devices/virtual/powercap/intel-rapl/enabled").expect("File not accessable");
+    let enabled = read_to_string("/sys/devices/virtual/powercap/intel-rapl/enabled").expect("File not accessable");
     if !enabled.starts_with("1") {
         println!("Powercap is not enabled!");
     }
 
-    let current_measured_uj_string = fs::read_to_string("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj").expect("Unable to read ujs");
+    let current_measured_uj_string = "0";
     //let current_measured_uj: i64 = current_measured_uj_string.parse::<i64>().unwrap();
-    let max_measured_uj_string = fs::read_to_string("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/max_energy_range_uj").expect("Unable to read max ujs");
+    let max_measured_uj_string = read_to_string("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/max_energy_range_uj").expect("Unable to read max ujs");
     //let max_measured_uj: i64 = max_measured_uj_string.parse().unwrap();
     println!("measured: {} out of {}", current_measured_uj_string, max_measured_uj_string);
 }
